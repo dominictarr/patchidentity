c704725b0ae2a6735ba0b7bbf45bc531d8f8eb52
Dominic Tarr <dominic.tarr@gmail.com>
2017-12-09 23:48:39 +1300

    support unbox and publish

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var ssbKeys = require('ssb-keys')
+var create = require('ssb-validate').create
 var path = require('path')
+var ref = require('ssb-ref')
 
 exports.gives = {
   identity: {
-    list: true, main: true, create: true, unbox: true
+    list: true, main: true, create: true, unbox: true, publish: true
   }
 }
 exports.needs = {
+  sbot: { getLatest: 'first', add: 'first' }
 //config ?
 }
 
-exports.create = function () {
+exports.create = function (api) {
   var keys = ssbKeys.loadOrCreateSync(path.join(process.env.HOME, '.ssb/secret'))
   //TODO: load all identites stored in ~/.ssb/identities/...
   return {
@@ -22,25 +25,46 @@ exports.create = function () {
       },
       unbox: function (data) {
         if('string' !== typeof data.value.content) return data
-        
         var content = ssbKeys.unbox(data.value.content, keys)
+        if(!content) return null
         var msg = data.value
         return {
           key: data.key,
           value: {
             previous: msg.previous,
-            author: msg.author,
             sequence: msg.sequence,
+            author: msg.author,
+            timestamp: msg.timestamp,
             hash: msg.hash,
             content: content,
+            private: true,
             signature: msg.signature
-          }
+          },
+          timestamp: data.timestamp
         }
+      },
+      publish: function (content, id, cb) {
+        if(!id) id = keys.id
+        if(id != keys.id) return cb(new Error('unknown identity'))
+        api.sbot.getLatest(id, function (err, data) {
+          var state = {
+            id: data.key,
+            sequence: data.value.sequence,
+            timestamp: data.value.timestamp,
+            queue: []
+          }
+          if(content.recps)
+            content = ssbKeys.box(content, content.recps.map(function (e) {
+              return ref.isFeed(e) ? e : e.link
+            }))
+
+          var msg = create(state, keys, null, content, Date.now())
+          api.sbot.add(msg, cb)
+        })
       }
     }
   }
 }
-
 
 
 
